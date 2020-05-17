e256c91ffe0a57e5b0c80f9cc677217dc52becd9
candinhojr <candinholuiz@gmail.com>
2020-05-17 17:08:05 -0300

    install and configure the marko dependency

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -16,15 +16,20 @@ module.exports = (app) => {
 
     app.get('/livros', (req, resp) => {
 
-        resp.send(`
-            <html>
-                <head>
-                    <meta charset='utf-8'>
-                </head>
-                <body>
-                    <h1>Listagem de livros do tio Candinho</h1>
-                </body>
-            </html>
-        `);
+        resp.marko(
+            require('../views/livros/lista/lista.marko'),
+            {
+                livros: [
+                    {
+                        id: 1,
+                        titulo: 'Fundamendos do Node'
+                    },
+                    {
+                        id: 2,
+                        titulo: 'Node avançado'
+                    }
+                ]
+            }
+        );
     });
 };
