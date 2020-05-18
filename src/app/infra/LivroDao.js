7d5b58b56f52e89e40ce09d8d27494383e9e723a
candinhojr <candinholuiz@gmail.com>
2020-05-18 17:03:24 -0300

    implement the methods find, update and delete

--- src/app/infra/LivroDao.js
+++ src/app/infra/LivroDao.js
@@ -12,37 +12,106 @@ class LivroDao {
                 'SELECT * FROM livros',
                 (erro, resultados) => {
                     if (erro) return reject('não foi possível listar os livros');
+                    
                     return resolve(resultados);
                 }
-            )
-        })
-    };
+            );
+        });
+    }
 
-    adiciona(livro) {
+    adiciona({titulo, preco, descricao}) {
 
         return new Promise((resolve, reject) => {
-            this._db.run(`
-                INSERT INTO livros (
+            this._db.run(
+                `
+                    INSERT INTO livros (
+                        titulo,
+                        preco,
+                        descricao
+                    ) values (?,?,?)
+                `,
+                [
                     titulo,
                     preco,
                     descricao
-                ) values (?,?,?)
-                `,
-                [
-                    livro.titulo,
-                    livro.preco,
-                    livro.descricao
                 ],
-                (erro) => {
+                erro => {
                     if (erro) {
                         console.log(erro);
                         return reject('Não foi possível adicionar o livro');
                     }
-                    
+
                     resolve();
-                })
-        })
-    };
+                }
+            );
+        });
+    }
+
+    atualiza({id, titulo, preco, descricao}) {
+
+        return new Promise((resolve, reject) => {
+            this._db.run(
+                `
+                    UPDATE livros
+                    SET 
+                        titulo = ?,
+                        preco = ?,
+                        descricao = ?
+                    WHERE id = ?
+                `,
+                [
+                    titulo,
+                    preco,
+                    descricao, 
+                    id
+                ],
+                erro => {
+                    if (erro) return reject('Não foi possível atualizar o livro');
+
+                    resolve();
+                }
+            );
+        });
+    }
+
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT * 
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) return reject('Não foi possível encontrar o livro');
+
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+    remove(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.run(
+                `
+                    DELETE 
+                    FROM livros 
+                    WHERE id = ?
+                `, 
+                [id],
+                erro => {
+                    if (erro) return reject('Não foi possível remover o livro');
+
+                    resolve();
+                }
+            );
+        });
+    }
+
 }
 
 module.exports = LivroDao;
