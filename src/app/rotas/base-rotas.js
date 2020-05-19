2d4cc67511cbc84c0d91400e29818b2538266c4a
candinhojr <candinholuiz@gmail.com>
2020-05-19 20:06:30 -0300

    refactoring the routes

---
+++ src/app/rotas/base-rotas.js
@@ -0,0 +1,8 @@
+const BaseControlador = require('../controladores/BaseControlador');
+const baseControlador = new BaseControlador();
+
+module.exports = (app) => {
+    const rotasBase = BaseControlador.rotas();
+
+    app.get(rotasBase.home, baseControlador.home());
+}
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,28 +1,7 @@
-const LivroControlador = require('../controladores/LivroControlador');
-const livroControlador = new LivroControlador();
-
-const BaseControlador = require('../controladores/BaseControlador');
-const baseControlador = new BaseControlador();
-
-const Livro = require('../modelos/livro');
+const baseRotas = require('./base-rotas');
+const livroRotas = require('./livro-rotas');
 
 module.exports = (app) => {
-    const rotasBase = BaseControlador.rotas();
-    const rotasLivro = LivroControlador.rotas();
-
-    app.get(rotasBase.home, baseControlador.home());
-
-    app.get(rotasLivro.lista, livroControlador.lista());
-
-    app.get(rotasLivro.cadastro, livroControlador.formularioCadastro());
-
-    app.get(rotasLivro.edicao, livroControlador.formularioEdicao());
-
-    app.post(rotasLivro.lista, Livro.validacoes(), livroControlador.cadastra());
-
-    app.put(rotasLivro.lista, livroControlador.edita());
-
-    app.delete(rotasLivro.delecao, livroControlador.remove());
-    
-    app.get(rotasLivro.delecao, livroControlador.detalha());
-};
+    baseRotas(app);
+    livroRotas(app);
+}
